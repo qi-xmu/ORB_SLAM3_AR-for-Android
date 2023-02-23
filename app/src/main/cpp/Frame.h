4aa53a2a5f633f40734600ff4dbf5d9fafc1e1cf
@奇 <360141773@qq.com>
2023-02-23 16:01:54 +0800

    Update Frame.h include path

--- app/src/main/cpp/Frame.h
+++ app/src/main/cpp/Frame.h
@@ -11,7 +11,7 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/video/tracking.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2\imgproc\types_c.h>
+#include <opencv2/imgproc/types_c.h>
 
 #include "Marker.h"
 
